b9c63a9a0d11c0e495e3e96723824a43a1a60a00
Juhyun Lee <leeju@korea.ac.kr>
2023-02-08 14:23:24 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dslearn',
+    version='0.0.1',
+    description='Tools for Data Science',
+    url='https://github.com/lee-ju/dslearn.git',
+    author='Juhyun Lee',
+    author_email='leeju@korea.ac.kr',
+    license='Juhyun Lee',
+    packages=['dslearn'],
+    zip_safe=False,
+    install_requires=[]
+)
